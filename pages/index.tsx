93adb1f61ca6c83bb032b11579dc5f17d6d7088a
sezze <9763776+sezze@users.noreply.github.com>
2021-03-14 23:42:16 +0200

    Add meta SEO tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,22 @@ export default function Home() {
       <Head>
         <title>Sebastian Aarnio</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="I'm a Software Engineering student at Åbo Akademi University (master's). I have an interest in web and game development. I currently spend most of my development time in TypeScript and C#."
+        />
+        <meta
+          name="keywords"
+          content="Sebastian Aarnio, Aarnio, Åbo, Turku, Developer"
+        />
+        <meta property="og:locale" content="fi_FI" />
+        <meta property="og:type" content="profile" />
+        <meta
+          property="og:description"
+          content="I'm a Software Engineering student at Åbo Akademi University (master's). I have an interest in web and game development. I currently spend most of my development time in TypeScript and C#."
+        />
+        <meta property="profile:first_name" content="Sebastian" />
+        <meta property="profile:last_name" content="Aarnio" />
       </Head>
 
       <main className={styles.main}>
